ad597bec3efc1666cd2d36a614b916109e9e5bcf
PrinceAutumn <841679171@qq.com>
2021-08-11 00:49:40 +0800

    Initial commit

---
+++ src/main/java/com/example/businesspartnerconfiguration/BusinessPartnerConfigurationApplication.java
@@ -0,0 +1,14 @@
+package com.example.businesspartnerconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessPartnerConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                BusinessPartnerConfigurationApplication.class, args);
+    }
+
+}
